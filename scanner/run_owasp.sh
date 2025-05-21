c4eafc78a45ee39f9766f87c781389fad49653a4
tobiasew <tobias.ewald@student.htw-berlin.de>
2025-05-21 12:17:10 +0200

    switch from gitlab to github // v2

--- scanner/run_owasp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run --rm -v $(pwd):/src owasp/dependency-check --scan /src --format JSON --out . --project OopsAI
-mv dependency-check-report.json owasp_report.json
--- scanner/run_trivy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run --rm -v $(pwd):/project aquasec/trivy fs /project --format json --output trivy_report.json
